9920a41d63f43ee7450a8bb3ad7b4dd23ea0743b
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-12 11:27:12 -0400

    start the formulas for Personal Expressions

--- AssociatingJS/Expressions_Personal/js/script.js
+++ AssociatingJS/Expressions_Personal/js/script.js
@@ -3,4 +3,17 @@
  * Expressions_Industry
 */
 
+
+alert("We need to see how many days in the year \nmy family can use the Annual Pass from SeaWorld?");       //What we are trying to figure out in the expression
+
+
+prompt("How many days a week does Stefani work?","5");
+
+
+
+
+
+
  alert("Test2");
+
+console.log("nothing");
