4da3ffc8e2835210b2ec819182075cadd0f60a84
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-12 12:12:18 -0400

    add more equations to expression, not solved yet

--- AssociatingJS/Expressions_Personal/js/script.js
+++ AssociatingJS/Expressions_Personal/js/script.js
@@ -7,13 +7,20 @@
 alert("We need to see how many days in the year \nmy family can use the Annual Pass from SeaWorld?");       //What we are trying to figure out in the expression
 
 
-prompt("How many days a week does Stefani work?","5");
+var wifeWork= prompt("How many days a week does my wife work?","5");
+
+var vacationWeek= prompt("How many weeks off a year does my wife get?");
+
+var vacationDays= vacationWeek*7;
+
+
+var totalDaysOff =(7- Number(wifeWork)) *(52-Number(vacationWeek)) + +Number(vacationDays);
 
 
 
 
 
 
- alert("Test2");
+console.log(totalDaysOff);
 
-console.log("nothing");
+//console.log(daysAvailable);
