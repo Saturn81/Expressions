dfdf3cecacb8a78b43f61c2c4ae210c013b5ab10
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-12 23:23:37 -0400

    create the Alert prompts, explains the problem

--- AssociatingJS/Expressions_Wacky/js/script.js
+++ AssociatingJS/Expressions_Wacky/js/script.js
@@ -5,10 +5,11 @@
 
 
 
-alert("Test3");
+alert("I put new sod down for my front lawn. \nThere is a alot of old grass, dirt, leaves \nand other lawn junk that needs to be disposed of.");       //describing the expression, this is the first part.
 
+alert("I find out that the garbage and lawn removal company will only pick up 15 bags of waste a week. \nProblem is there lots of bags of lawn waste.")  //The second part explaining the expression, its a long story so I need to break it up.
 
-
+alert("I have to see how many weeks it will take to get rid of all the lawn waste I have collected in trash bags......")  //last part, but most important, it states I need to see how many weeks I need to wait till all my lawn trash bags are picked up.
 
 
 console.log("This is a test");
