727a34c2d1a9c0adb594b900a2a77cd041428097
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-13 00:28:27 -0400

    fix problem with the format of the expression, and create prompts

--- AssociatingJS/Expressions_Wacky/js/script.js
+++ AssociatingJS/Expressions_Wacky/js/script.js
@@ -5,11 +5,25 @@
 
 
 
-alert("I put new sod down for my front lawn. \nThere is a alot of old grass, dirt, leaves \nand other lawn junk that needs to be disposed of.");       //describing the expression, this is the first part.
+alert("I put new sod down for my front lawn. \nThere is a lot of old grass, dirt, leaves \nand other lawn junk that needs to be disposed of.");       //describing the expression, this is the first part.
 
 alert("I find out that the garbage and lawn removal company will only pick up 15 bags of waste a week. \nProblem is there lots of bags of lawn waste.")  //The second part explaining the expression, its a long story so I need to break it up.
 
 alert("I have to see how many weeks it will take to get rid of all the lawn waste I have collected in trash bags......")  //last part, but most important, it states I need to see how many weeks I need to wait till all my lawn trash bags are picked up.
+
+
+var bagsTrash= prompt("How many trash bags of sod did I use for the lawn","75");
+
+var truckFriend= prompt("How many times can I borrow my friends truck and haul to the dump (truck can hold 20 bags of trash?)");
+
+var Car= prompt("How many times do I use my own car and drive to the dump?(my car can hold 12 bags of trash)");
+
+
+
+
+
+
+
 
 
 console.log("This is a test");
