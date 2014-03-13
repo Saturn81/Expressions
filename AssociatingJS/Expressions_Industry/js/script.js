a79d49be78f854ccaa4d5fdd852802662208650c
Jeremy Andrew Suika <jsuika2@fullsail.edu>
2014-03-13 11:37:28 -0400

    expressions_Industry, work on Array;

--- AssociatingJS/Expressions_Industry/js/script.js
+++ AssociatingJS/Expressions_Industry/js/script.js
@@ -8,9 +8,12 @@
  //for their time spent.
 
 
-var blake = prompt("How many hours did blake work on project?");
-var john= prompt("How many hours did John work on the project");
-var 
+var blake = prompt("How many hours did blake work on project?\n (junior agents get $20.00 a hour)");
+var john= prompt("How many hours did John work on the project?\n (junior agents get $20.00 a hour)");
+var jane = prompt("How many hours did Jane work on the project?\n (junior agents get $20.00 a hour)");
+var outSource= prompt("How many outside temp agents did you hire?\n (Each agent gets a flat $500.00 for work done)")
+
+var workers =[blake, john, jane, outSource];
 
 
-console.log(blake);
+console.log(Number(workers[2])+ //Number(workers[1]));
